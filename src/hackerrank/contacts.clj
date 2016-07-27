49982680b5d7311954b779760c525ed395d7e469
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-07-27 13:52:39 -0400

    fat trie solution

--- src/hackerrank/contacts.clj
+++ src/hackerrank/contacts.clj
@@ -1,58 +1,22 @@
 (ns hackerrank.contacts)
 
-(def count-index 26)
-(def word-index 27)
-
-(defn make-jtrie
-  []
-  (let [a (make-array java.lang.Object 28)]
-    (aset a count-index 0)
-    a))
-
-(defn char->index
-  [c]
-  (- (int c)
-     (int \a)))
-
-(defn jtrie-lookup-prefix
-  [trie prefix]
-  (if (or (empty? prefix)
-          (nil? trie))
-    trie
-    (let [index (char->index (first prefix))]
-      (recur (aget trie index)
-             (rest prefix)))))
-
-(defn jtrie-word?
+(defn add-to-trie
   [trie word]
-  (let [trie (jtrie-lookup-prefix trie word)]
-    (and (not (nil? trie))
-         (not (nil? (aget trie word-index))))))
+  (let []
+    (if (nil? (get-in trie [word :word] nil))
+      (let [trie (reduce (fn [trie prefix]
+                           (update-in trie [prefix :count] #((fnil inc 0) %)))
+                         trie
+                         (for [n (range (count word))]
+                             (.substring word 0 (inc n))))]
+        (assoc trie word
+               (merge (get trie word)
+                      {:word word})))
+      trie)))
 
-(defn add-to-jtrie
+(defn prefix-count
   [trie word]
-  (when-not (jtrie-word? trie word) 
-    (loop [trie trie
-           word word]
-      (aset trie count-index (inc (aget trie count-index)))
-      (when-not (empty? word)      
-        (let [[c remaining] word
-              index (char->index c)]
-          (when (nil? (aget trie index))
-            (aset trie index (make-jtrie)))
-          (recur (aget trie index)
-                 (rest word)))))
-    (aset (jtrie-lookup-prefix trie word)
-        word-index true)
-    nil))
-
-(defn jtrie-prefix-count
-  [trie prefix]
-  (or (when-not (nil? trie)
-        (let [trie (jtrie-lookup-prefix trie prefix)]
-          (when-not (nil? trie)
-            (aget trie count-index))))
-      0))
+  (get-in trie [word :count] 0))
 
 (defn process-line
   [trie line]
@@ -61,13 +25,13 @@
       (let [[cmd word] args
             word (str word)]
         (case cmd
-          add (do (add-to-jtrie trie word)
+          add (do (swap! trie add-to-trie word)
                   nil)
-          find (jtrie-prefix-count trie word))))))
+          find (prefix-count @trie word))))))
 
 (defn solve
   [in]
-  (let [trie (make-jtrie)
+  (let [trie (atom {})
         process (comp
                  (map #(process-line trie %))
                  (filter #(not (nil? %))))
@@ -85,26 +49,3 @@
           "resources/input02.txt"
           ;;"resources/test-input.txt"
           )))
-
-
-;; (defn add-to-trie
-;;   [trie word]
-;;   (let [word (sequence word)]
-;;     (if (nil? (get-in trie (conj (vec word) :word) nil))
-;;       (let [trie (reduce (fn [trie keys]
-;;                            (update-in trie (conj (vec keys) :count) #((fnil inc 0) %)))
-;;                          trie
-;;                          (let [keys word]
-;;                            (for [n (range (count keys))]
-;;                              (take (+ 1 n) keys))))]
-;;         (assoc-in trie word
-;;                   (merge (get-in trie word)
-;;                          {:word true})))
-;;       trie)))
-
-;; (defn prefix-count
-;;   [trie word]
-;;   (or (:count (get-in trie (sequence word)))
-;;       0))
-
-
